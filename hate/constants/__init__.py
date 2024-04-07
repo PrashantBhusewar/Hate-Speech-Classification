ebfb11103de9df5202216e3bc3edcca622068bc4
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-07 16:51:40 +0530

    data transformation

--- hate/constants/__init__.py
+++ hate/constants/__init__.py
@@ -15,3 +15,22 @@ TWEET = 'tweet'
 DATA_INGESTION_ARTIFACTS_DIR = "DataIngestionArtifacts"
 DATA_INGESTION_IMBALANCE_DATA_DIR = "imbalanced_data.csv"
 DATA_INGESTION_RAW_DATA_DIR = "raw_data.csv"
+
+
+# Data transformation constants 
+DATA_TRANSFORMATION_ARTIFACTS_DIR = 'DataTransformationArtifacts'
+TRANSFORMED_FILE_NAME = "final.csv"
+DATA_DIR = "data"
+ID = 'id'
+AXIS = 1
+INPLACE = True
+DROP_COLUMNS = ['Unnamed: 0','count','hate_speech','offensive_language','neither']
+CLASS = 'class'
+
+
+# Model training constants
+MODEL_TRAINER_ARTIFACTS_DIR = 'ModelTrainerArtifacts'
+TRAINED_MODEL_DIR = 'trained_model'
+TRAINED_MODEL_NAME = 'model.h5'
+X_TEST_FILE_NAME = 'x_test.csv'
+Y_TEST_FILE_NAME = 'y_test.csv'
