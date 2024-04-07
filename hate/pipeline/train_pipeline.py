43790d4b13f8bde62e2496c37ff2b03e9fe073d2
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-07 17:41:17 +0530

    model pusher

--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -5,18 +5,19 @@ from hate.components.data_ingestion import DataIngestion
 from hate.components.data_transforamation import DataTransformation
 from hate.components.model_trainer import ModelTrainer
 from hate.components.model_evaluation import ModelEvaluation
-
-
+from hate.components.model_pusher import ModelPusher
 
 from hate.entity.config_entity import (DataIngestionConfig,
                                        DataTransformationConfig,
                                        ModelTrainerConfig,
-                                       ModelEvaluationConfig)
+                                       ModelEvaluationConfig,
+                                       ModelPusherConfig)
 
 from hate.entity.artifact_entity import (DataIngestionArtifacts,
                                          DataTransformationArtifacts,
                                          ModelTrainerArtifacts,
-                                         ModelEvaluationArtifacts)
+                                         ModelEvaluationArtifacts,
+                                         ModelPusherArtifacts)
 
 
 class TrainPipeline:
@@ -25,9 +26,9 @@ class TrainPipeline:
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
         self.model_evaluation_config =ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
 
 
-    
     def start_data_ingestion(self) -> DataIngestionArtifacts:
         logging.info("Entered the start_data_ingestion method of TrainPipeline class")
         try:
@@ -42,7 +43,7 @@ class TrainPipeline:
         except Exception as e:
             raise HateException(e, sys) from e
         
-
+    
     def start_data_transformation(self, data_ingestion_artifacts = DataIngestionArtifacts) -> DataTransformationArtifacts:
         logging.info("Entered the start_data_transformation method of TrainPipeline class")
         try:
@@ -74,8 +75,8 @@ class TrainPipeline:
 
         except Exception as e:
             raise HateException(e, sys) 
-
-
+        
+    
     def start_model_evaluation(self, model_trainer_artifacts: ModelTrainerArtifacts, data_transformation_artifacts: DataTransformationArtifacts) -> ModelEvaluationArtifacts:
         logging.info("Entered the start_model_evaluation method of TrainPipeline class")
         try:
@@ -89,12 +90,28 @@ class TrainPipeline:
 
         except Exception as e:
             raise HateException(e, sys) from e
+        
+    
+    def start_model_pusher(self,) -> ModelPusherArtifacts:
+        logging.info("Entered the start_model_pusher method of TrainPipeline class")
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.model_pusher_config,
+            )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            logging.info("Initiated the model pusher")
+            logging.info("Exited the start_model_pusher method of TrainPipeline class")
+            return model_pusher_artifact
+
+        except Exception as e:
+            raise HateException(e, sys) from e
 
 
     def run_pipeline(self):
         logging.info("Entered the run_pipeline method of TrainPipeline class")
         try:
             data_ingestion_artifacts = self.start_data_ingestion()
+
             data_transformation_artifacts = self.start_data_transformation(
                 data_ingestion_artifacts=data_ingestion_artifacts
             )
@@ -103,7 +120,11 @@ class TrainPipeline:
             )
             model_evaluation_artifacts = self.start_model_evaluation(model_trainer_artifacts=model_trainer_artifacts,
                                                                     data_transformation_artifacts=data_transformation_artifacts
-            )
+            ) 
+            if not model_evaluation_artifacts.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
+            
+            model_pusher_artifacts = self.start_model_pusher()
 
             logging.info("Exited the run_pipeline method of TrainPipeline class") 
 
