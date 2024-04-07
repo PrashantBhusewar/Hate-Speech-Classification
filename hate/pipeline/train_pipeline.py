dfb7e32a152768b60812dc807748b6ee972a628a
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-07 17:16:47 +0530

    model evaluation

--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -4,16 +4,19 @@ from hate.exception import HateException
 from hate.components.data_ingestion import DataIngestion
 from hate.components.data_transforamation import DataTransformation
 from hate.components.model_trainer import ModelTrainer
+from hate.components.model_evaluation import ModelEvaluation
 
 
 
 from hate.entity.config_entity import (DataIngestionConfig,
                                        DataTransformationConfig,
-                                       ModelTrainerConfig)
+                                       ModelTrainerConfig,
+                                       ModelEvaluationConfig)
 
 from hate.entity.artifact_entity import (DataIngestionArtifacts,
                                          DataTransformationArtifacts,
-                                         ModelTrainerArtifacts)
+                                         ModelTrainerArtifacts,
+                                         ModelEvaluationArtifacts)
 
 
 class TrainPipeline:
@@ -21,6 +24,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config =ModelEvaluationConfig()
 
 
     
@@ -72,6 +76,21 @@ class TrainPipeline:
             raise HateException(e, sys) 
 
 
+    def start_model_evaluation(self, model_trainer_artifacts: ModelTrainerArtifacts, data_transformation_artifacts: DataTransformationArtifacts) -> ModelEvaluationArtifacts:
+        logging.info("Entered the start_model_evaluation method of TrainPipeline class")
+        try:
+            model_evaluation = ModelEvaluation(data_transformation_artifacts = data_transformation_artifacts,
+                                                model_evaluation_config=self.model_evaluation_config,
+                                                model_trainer_artifacts=model_trainer_artifacts)
+
+            model_evaluation_artifacts = model_evaluation.initiate_model_evaluation()
+            logging.info("Exited the start_model_evaluation method of TrainPipeline class")
+            return model_evaluation_artifacts
+
+        except Exception as e:
+            raise HateException(e, sys) from e
+
+
     def run_pipeline(self):
         logging.info("Entered the run_pipeline method of TrainPipeline class")
         try:
@@ -81,6 +100,9 @@ class TrainPipeline:
             )
             model_trainer_artifacts = self.start_model_trainer(
                 data_transformation_artifacts=data_transformation_artifacts
+            )
+            model_evaluation_artifacts = self.start_model_evaluation(model_trainer_artifacts=model_trainer_artifacts,
+                                                                    data_transformation_artifacts=data_transformation_artifacts
             )
 
             logging.info("Exited the run_pipeline method of TrainPipeline class") 
