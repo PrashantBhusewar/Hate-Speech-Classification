863b5ac6089dced131489c162aa3110dab8a45e8
PrashantBhusewar <prashantbhusewar5@gmail.com>
2024-04-07 17:06:22 +0530

    model trainer

--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -3,20 +3,24 @@ from hate.logger import logging
 from hate.exception import HateException
 from hate.components.data_ingestion import DataIngestion
 from hate.components.data_transforamation import DataTransformation
+from hate.components.model_trainer import ModelTrainer
 
 
 
 from hate.entity.config_entity import (DataIngestionConfig,
-                                       DataTransformationConfig)
+                                       DataTransformationConfig,
+                                       ModelTrainerConfig)
 
 from hate.entity.artifact_entity import (DataIngestionArtifacts,
-                                         DataTransformationArtifacts)
+                                         DataTransformationArtifacts,
+                                         ModelTrainerArtifacts)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -52,12 +56,31 @@ class TrainPipeline:
             raise HateException(e, sys) from e
         
 
+    def start_model_trainer(self, data_transformation_artifacts: DataTransformationArtifacts) -> ModelTrainerArtifacts:
+        logging.info(
+            "Entered the start_model_trainer method of TrainPipeline class"
+        )
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifacts=data_transformation_artifacts,
+                                        model_trainer_config=self.model_trainer_config
+                                        )
+            model_trainer_artifacts = model_trainer.initiate_model_trainer()
+            logging.info("Exited the start_model_trainer method of TrainPipeline class")
+            return model_trainer_artifacts
+
+        except Exception as e:
+            raise HateException(e, sys) 
+
+
     def run_pipeline(self):
         logging.info("Entered the run_pipeline method of TrainPipeline class")
         try:
             data_ingestion_artifacts = self.start_data_ingestion()
             data_transformation_artifacts = self.start_data_transformation(
                 data_ingestion_artifacts=data_ingestion_artifacts
+            )
+            model_trainer_artifacts = self.start_model_trainer(
+                data_transformation_artifacts=data_transformation_artifacts
             )
 
             logging.info("Exited the run_pipeline method of TrainPipeline class") 
